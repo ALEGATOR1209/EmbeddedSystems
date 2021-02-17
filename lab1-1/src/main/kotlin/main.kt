fa2be18ee9869e42025e920db5e1b4f1ffa08de9
Oleh Kovalyshyn <kovalyshinO@gmail.com>
2021-02-17 23:04:25 +0200

    Fixed csv file position

--- lab1-1/src/main/kotlin/main.kt
+++ lab1-1/src/main/kotlin/main.kt
@@ -3,11 +3,11 @@ fun main() {
     plotSignal(14, 1700, 64)
     benchmark(
         nMin = 1,
-        nMax = 50_001,
+        nMax = 1_000_001,
         nStep = 5000,
         wMax = 1700,
-        numMin = 1,
-        numMax = 50_001,
-        "res/out.csv"
+        numMin = 64,
+        numMax = 64,
+        "lab1-1/res/out.csv"
     )
 }
